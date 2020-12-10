b8b43b114f52d4be5746e0e855624329185f370f
maxueliang <644766713@qq.com>
2020-12-10 20:07:32 +0800

    feta: Vssue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,19 @@ module.exports = {
         ['meta', { name: 'author', content: '世界不浪漫' }],
         ['meta', { name: 'keywords', content: 'vuepress 介绍 vuepress 说明, 世界不浪漫' }]
       ],
+    plugins: {
+        '@vssue/vuepress-plugin-vssue': {
+          // set `platform` rather than `api`
+          platform: 'github-v4',
+    
+          // all other options of Vssue are allowed
+          owner: 'maxueliang',
+          repo: 'vuepress-starter',
+          clientId: '523c59b2473ad1b572c6',
+          clientSecret: '9cf0688293e423bed554c9ec9409c36c51876dd0',
+          autoCreateIssue: true
+        },
+      },  
     themeConfig: {
       lastUpdated: '更新时间',
       logo: '/assets/img/head.png',
